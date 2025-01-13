8a5e985aa8a3b1a436d0a6743c059b115cde7ab3
Chanhyeok Leem <imcksgur@naver.com>
2025-01-13 17:14:24 +0900

    feat: download youtube8m videos & audios

--- dataset/download_video_audio.py
+++ dataset/download_video_audio.py
@@ -44,7 +44,7 @@ def download_videos_and_audio(file_path, video_folder="video", audio_folder="aud
         print(f"An unexpected error occurred: {e}")
 
 # Example usage
-input_file = "/Users/imch/workspace/youtube-8m-videos-frames/category-ids/movie_urls.txt"  # Replace with the name of your URL file
+input_file = "/data/ephemeral/home/level4-cv-finalproject-hackathon-cv-15-lv3/dataset/movie_urls.txt"  # Replace with the name of your URL file
 video_folder = "video"  # Folder to store videos
 audio_folder = "audio"  # Folder to store audio files
 download_videos_and_audio(input_file, video_folder, audio_folder)
